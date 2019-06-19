292566d7f7dea6a83b4aab2a407d9b4af4fc99c6
Alexander Santos <39683032+EminentCoding@users.noreply.github.com>
2019-06-19 18:26:07 -0400

    Finished the jquery login form

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 // https://github.com/oakmac/jquery-login-form
 
 var express = require('express')
+var fileSystem = require('fs')
 var bodyParser = require('body-parser')
 
 var app = express()
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 const defaultPort = 7979
@@ -15,6 +16,7 @@ app.use(enableCors)
 app.use(slowItDown)
 app.use(express.static('public'))
 app.post('/api/login', loginRequest)
+app.post('/api/login-help', userAnalytics)
 app.listen(port)
 
 console.log('HTTP server running on port ' + port)
@@ -23,63 +25,80 @@ console.log('HTTP server running on port ' + port)
 // Functions
 // -----------------------------------------------------------------------------
 
-function randomInt (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-function slowItDown (req, res, nextFn) {
-  if (req.query && req.query['_slow']) {
-    setTimeout(nextFn, randomInt(400, 1000))
-  } else {
+function slowItDown(req, res, nextFn) {
+    if (req.query && req.query['_slow']) {
+        setTimeout(nextFn, randomInt(400, 1000))
+    } else {
+        nextFn()
+    }
+}
+
+function enableCors(req, res, nextFn) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     nextFn()
-  }
-}
-
-function enableCors (req, res, nextFn) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  nextFn()
 }
 
 const testUsers = {
-  'testuser1': 'ilovebananas',
-  'testadmin5': 'always_name_your_functions',
-  'qauser3': 'luv2manage5tate'
+    'testuser1': 'ilovebananas',
+    'testadmin5': 'always_name_your_functions',
+    'qauser3': 'luv2manage5tate'
 }
 
-function loginRequestError (req, res) {
-  res.status(500)
-     .send(JSON.stringify({error: 'Server error :('}))
+function loginRequestError(req, res) {
+    res.status(500)
+        .send(JSON.stringify({ error: 'Server error :(' }))
 }
 
-function loginRequestResponse (req, res) {
-  var username = req.body.username
-  var password = req.body.password
+function loginRequestResponse(req, res) {
+    var username = req.body.username
+    var password = req.body.password
 
-  if (!testUsers.hasOwnProperty(username)) {
-    res.status(400)
-       .send(JSON.stringify({error: 'Invalid username.'}))
-  } else if (testUsers[username] !== password) {
-    res.status(400)
-       .send(JSON.stringify({error: 'Invalid password.'}))
-  } else {
-    res.status(200)
-       .send(JSON.stringify({message: 'Login successful!'}))
-  }
+    if (!testUsers.hasOwnProperty(username)) {
+        res.status(400)
+            .send(JSON.stringify({ error: 'Invalid username.' }))
+    } else if (testUsers[username] !== password) {
+        res.status(400)
+            .send(JSON.stringify({ error: 'Invalid password.' }))
+    } else {
+        res.status(200)
+            .send(JSON.stringify({ message: 'Login successful!' }))
+    }
 }
 
-function loginRequest (req, res) {
-  var dice = randomInt(1, 10)
-  var normalWait = randomInt(150, 3500)
-  var longWait = randomInt(20 * 1000, 40 * 1000)
-  var normalResponse = loginRequestResponse.bind(null, req, res)
-  var errorResponse = loginRequestError.bind(null, req, res)
+function loginRequest(req, res) {
+    var dice = randomInt(1, 10)
+    var normalWait = randomInt(150, 3500)
+    var longWait = randomInt(20 * 1000, 40 * 1000)
+    var normalResponse = loginRequestResponse.bind(null, req, res)
+    var errorResponse = loginRequestError.bind(null, req, res)
 
-  if (dice === 1) { // 1/10 requests take 20-40 seconds to respond
-    setTimeout(normalResponse, longWait)
-  } else if (dice === 2 || dice === 3) { // 2/10 requests fail with a 500 error
-    setTimeout(errorResponse, normalWait)
-  } else { // the rest of the requests are handled normally after a brief wait time
-    setTimeout(normalResponse, normalWait)
-  }
+    if (dice === 1) { // 1/10 requests take 20-40 seconds to respond
+        setTimeout(normalResponse, longWait)
+    } else if (dice === 2 || dice === 3) { // 2/10 requests fail with a 500 error
+        setTimeout(errorResponse, normalWait)
+    } else { // the rest of the requests are handled normally after a brief wait time
+        setTimeout(normalResponse, normalWait)
+    }
+}
+
+function userAnalytics(req, res) {
+    var username = req.body.username;
+    var userStats = JSON.parse(req.body.userData);
+    //Use FS to first read the userData then it writes all of our analytics to it.
+
+    fileSystem.readFile('userData.json', (err, content) => {
+        if (err) throw err;
+        var parseJson = JSON.parse(content);
+        //Using the username as a key
+        parseJson.helpModal[username] = userStats;
+        fileSystem.writeFile('userData.json', JSON.stringify(parseJson), function(err) {
+            if (err) throw err;
+        })
+    })
+
 }
--- public/main.js
+++ public/main.js
@@ -1,55 +1,87 @@
+const apiURL = `http://127.0.0.1:7979/`;
 
-const apiURL = `http://127.0.0.1:7979/`
 
-$('.login-form').submit(()=>{
-    $.post(`${apiURL}api/login`, 
-            {
-                username: $('#loginInput').val(), 
-                password: $('#passwordInput').val()
-            }
-        )
-        .done((successData) => {
-            console.log(successData);
+//The site will gather some user analytics for the events that a session consist of.
+let failedLogin = 0;
+let userAnalytics = {};
+userAnalytics.helpModalWasOpened = false;
+
+
+$('.big-input').click(() => {
+    $('#serverResponse').html("");
+})
+
+//This is a simple login form.
+$('.login-form').submit(() => {
+    $('#serverResponse').html("");
+    $('.action-row').html("Loading....")
+    let responseTime = new Date().getTime()
+    $.post(`${apiURL}api/login`, {
+            username: $('#loginInput').val(),
+            password: $('#passwordInput').val()
+        })
+        .done((result) => {
+            $('.action-row').html('<button class="primary-btn" id="loginBtn">Log In</button>');
+            userAnalytics.secondsTookToLogin = Math.floor((new Date().getTime() - responseTime / 1000) % 60)
             $('#serverResponse').html("Success!");
-            location.href='./index.html'
+            $.post(`${apiURL}api/login-help`, {
+                    username: $('#loginInput').val(),
+                    userData: JSON.stringify(userAnalytics)
+
+                })
+                .done((res) => {
+                    console.log(res, userAnalytics)
+                })
+            location.href = 'index.html'
         })
         .fail(result => {
-            if(result.status === 400){
-                $('#serverResponse').html("Wrong credentials")
-            }else{
-                $('#serverResponse').html("Server Error!")
-            }
+            failedLogin += 1;
+            userAnalytics.numFailedLoginAttempts = failedLogin;
+            responseError = JSON.parse(result.responseText)
+            $('#serverResponse').html(responseError.error);
         });
-    }
-);
+});
 
-$(document).on({
-    ajaxStart: function(){$('.action-row').html("Loading....")},
-    ajaxStop: function () {$('.action-row').html('<button class="primary-btn disabled" id="loginBtn">Log In</button>');}
-})
 
-function randomInt (min, max) {
+// $(document).on({
+//     ajaxStart: function() { $('.action-row').html("Loading...."); },
+//     ajaxStop: function() { $('.action-row').html('<button class="primary-btn" id="loginBtn">Log In</button>'); }
+// });
+
+
+
+function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
-  }
+};
 
-let modal = document.getElementById('myModal')
-let modalBtn = document.getElementById('modalBtn');
-let span = document.getElementsByClassName('close')[0];
-let modalText = document.getElementById('modalText')
+
+
+
+//Modal code to toggle the modal's visibility & content
+//Above are all the html element targeting the modal.
+//The modal will tracks some analytics for the page.
+const modal = document.getElementById('myModal');
+const modalBtn = document.getElementById('modalBtn');
+const span = document.getElementsByClassName('close')[0];
+const modalText = document.getElementById('modalText');
+
 modalBtn.addEventListener('click', () => {
-     let N = randomInt(1, 4)
+    let N = randomInt(1, 4)
+    userAnalytics.helpModalWasOpened = true;
+    userAnalytics.helpContentNum = N;
+
     $.get(`${apiURL}/md/login-help-${N}.md`)
-    .then(result => {
-        console.log(N)
-        modalText.innerHTML = marked(`${result}`)
-    })
-    .then(modal.style.display = "block")
-})
-span.addEventListener('click', () =>{
+        .then(result => {
+            console.log(N)
+            modalText.innerHTML = marked(`${result}`)
+        })
+        .then(modal.style.display = "block")
+});
+span.addEventListener('click', () => {
     modal.style.display = "none"
-})
-window.onclick = (event) =>{
-    if(event.target == modal) {
+});
+window.onclick = (event) => {
+    if (event.target == modal) {
         modal.style.display = "none"
     }
-}
+};
